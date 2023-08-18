568b7d552d3a481b5910f53584a8420fdf4d7391
nhanien <141650994+nhanien@users.noreply.github.com>
2023-08-18 22:39:50 +0700

    Update nhannhi.js

--- nhannhi.js
+++ nhannhi.js
@@ -1,11 +1,10 @@
 function click_cancel() {
-    window.location ="indexx.html"
+    window.location ="tt.html"
 }
 
 
 function init() {
     var cancel =document.getElementById('cancel');
     cancel.onclick =click_cancel;
-    alert("123")
 }
 window.onload=init;
